9012f55bda705cb0e21ec20daec9066313114ec3
Rajesh Khatwani <khatwanir111@gmail.com>
2025-06-07 22:32:14 +0530

    Create datetime.py

---
+++ datetime.py
@@ -0,0 +1,27 @@
+"""
+A script to show the user the current time.
+
+"""
+
+# import the required modules
+import datetime
+
+
+def gettime():
+    """
+    A function to return the current time.
+
+    Returns
+    -------
+    tuple:
+        A tuple containing the hour, minutes and seconds.
+    """
+
+    now = datetime.datetime.now()
+
+    return now.hour, now.minute, now.second + 1e-6 * now.microsecond
+
+# get the time
+hour, minute, seconds = gettime()
+
+print(f"The current time is {hour}:{minute}:{seconds}")
